be94d9d1b12499cc4296f02a18a63aa8a0af000d
Andrei Balan <andrei.balan98@gmail.com>
2025-03-13 00:05:36 +0200

    modifications

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        
+        String[] languages = {"Java", "C#", "Python", "PHP"};
+
+        for (int i = 0; i < languages.length; i++) {
+            System.out.print((int) (Math.random() * 10_000) + " ");
+        }
+        System.out.println();
+
+        int n = (int) (Math.random() * 10_000);
+        n += 0b10101;
+        System.out.println(n);
+
+        n += 0xFF;
+        System.out.println(n);
+
+        int aux;
+        while (n > 9) {
+            aux = 0;
+            while (n != 0){
+                aux +=  n % 10;
+                n = n / 10;
+            }
+            n = aux;
+        }
+        System.out.println(n);
     }
 }
